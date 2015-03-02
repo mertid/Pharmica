db47846d834f72c9896d13931d900b7f5f1079f6
Merritt Tidwell <merritt.tidwell@gmail.com>
2015-03-02 15:18:17 -0500

    studiesTVC and SB

---
+++ pharmica/StudiesTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  StudiesTableViewController.h
+//  pharmica
+//
+//  Created by Merritt Tidwell on 3/2/15.
+//  Copyright (c) 2015 Merritt Tidwell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StudiesTableViewController : UITableViewController
+
+@end
