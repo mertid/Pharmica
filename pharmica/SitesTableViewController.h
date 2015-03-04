dcbd03a3e9151603a2f2ea87a40a930e0553259b
Merritt Tidwell <merritt.tidwell@gmail.com>
2015-03-04 15:12:16 -0500

    updates to story board and placeholders

---
+++ pharmica/SitesTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  SitesTableViewController.h
+//  pharmica
+//
+//  Created by Merritt Tidwell on 3/4/15.
+//  Copyright (c) 2015 Merritt Tidwell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SitesTableViewController : UITableViewController
+
+@end
---
+++ pharmica/SitesTableViewController.m
@@ -0,0 +1,90 @@
+//
+//  SitesTableViewController.m
+//  pharmica
+//
+//  Created by Merritt Tidwell on 3/4/15.
+//  Copyright (c) 2015 Merritt Tidwell. All rights reserved.
+//
+
+#import "SitesTableViewController.h"
+
+@interface SitesTableViewController ()
+
+@end
+
+@implementation SitesTableViewController {
+
+    NSArray * siteData;
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    siteData = @[@"California",
+                 @"Georgia",
+                 @"New York"];
+
+}
+
+
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    // Return the number of sections.
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    // Return the number of rows in the section.
+    return siteData.count;
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell" forIndexPath:indexPath];
+    
+    cell.textLabel.text = siteData[indexPath.row];
+    return cell;
+}
+
+
+
+
+/*
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the row from the data source
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }   
+}
+*/
+
+/*
+// Override to support rearranging the table view.
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
+}
+*/
+
+/*
+// Override to support conditional rearranging of the table view.
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the item to be re-orderable.
+    return YES;
+}
+*/
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
